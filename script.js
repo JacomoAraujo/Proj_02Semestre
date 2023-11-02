9503a2f8cadd199d87d663e9184a6d539887b173
Jacomo Araujo <jacomo68araujo@gmail.com>
2023-11-02 13:38:59 -0300

    Funçoes Api Rodando

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 async function buscaPokemon(){
-    const nomePokemon = document.getElementById('nomePokemon').value
-    const url = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`
+    const nomePokemon = document.getElementById('nomePokemon').value;
+    const url = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`;
     try{
-        const response = await fetch (url)
-        const data = await response.json()
-        //altura.value=data.
-        altura.value = data.weight
-        peso.value = data.heigth
-        nome.value = data.name
+        const response = await fetch (url);
+        const data = await response.json();
 
-        console.log(data)
+        const peso = document.getElementById('peso');
+        const nomePoke = document.getElementById('nomePoke');
+        const altura = document.getElementById('altura');
+        const nmrPoke = document.getElementById('nmrPoke');
+        const imagemUrl = document.getElementById("fotoPokemon")
+        
+        altura.textContent = `Altura; ${data.weight}Cm`;
+        peso.textContent = `Peso: ${data.height}Kg`;
+        nmrPoke.textContent = `Índice Pokedex: ${data.order}º`;
+        nomePoke.textContent = `${data.name}`;
+        imagemUrl.src = data.sprites.front_default;
+
+        
+
+
+
+        //console.log(data)
     }
     catch (error) {
         console.error(error)
         return null
     }
-        const peso = document.getElementById('peso')
-        const nome = document.getElementById('nome')
-        const altura = document.getElementById('altura')
+
 }
